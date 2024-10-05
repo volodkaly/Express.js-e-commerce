d986a7f36c76a02b7802f211664e3f73a72f0656
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2024-10-05 11:50:40 +0200

    err handling

--- app.js
+++ app.js
@@ -175,7 +175,8 @@ app.use('/', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+ res.render('error');
+ next();
 });
 
 // error handler
